4db34498b705668767957d10bc5e12d7f06b38ee
EvenyGrinko <john_forever_young@mail.ru>
2020-07-06 16:47:43 +0300

    Improve search and addition functionality

--- index.js
+++ index.js
@@ -1,117 +1,139 @@
 const initialItems = ["Wake up at 7 o'clock", "Read book", "Go to gym"];
 
-let allItems = []; //all items: "completed" + "active"
-let activeItems = []; //i.e. not completed yet items
+const allItems = []; //all items: "completed" + "active"
+const activeItems = new Set(); //i.e. not completed yet items
+const completedItems = new Set(); //completed items
+const foundItems = new Set(); //items that were found by search
 
-const completedItems = []; //completed items
-const foundItems = []; //items that were found by search
-
-let focusedItem;
+let focusedBtn; //reference to the button, that has class "focused"
 const list = document.querySelector(".list");
 const inputElem = document.getElementById("inputNewElem");
 const searchElem = document.getElementById("searchElem");
-const btnAllItems = document.querySelector(".btn.allItemsColor");
-const btnCompletedItems = document.querySelector(".btn.completedItemsColor");
-const btnActiveItems = document.querySelector(".btn.activeItemsColor");
+const btnAllItems = document.getElementById("btnAllItems");
+const btnActiveItems = document.getElementById("btnActiveItems");
+const btnCompletedItems = document.getElementById("btnCompletedItems");
+
 //Add initial default items to the list, when the DOM fully loaded
 document.addEventListener("DOMContentLoaded", () => {
   btnAllItems.classList.toggle("focused");
-  focusedItem = btnAllItems;
-  if (activeItems.length === 0) {
-    activeItems = [...initialItems];
-    allItems = [...initialItems];
-    renderItems(activeItems);
-  }
+  focusedBtn = btnAllItems;
+  initialItems.forEach((item, index) => {
+    allItems.push(getNewItem(item, index, "activeItemsColor"));
+    activeItems.add(index);
+  });
+  renderItemsOn(focusedBtn.id);
 });
 
 //Event listener for the "Add new item" input
 inputElem.addEventListener("keyup", (event) => {
   if (event.keyCode === 13) {
     const itemText = event.target.value;
-    allItems.push(itemText);
-    activeItems.length === 0
-      ? (activeItems = [...activeItems])
-      : activeItems.push(itemText);
-
-    addItem(getNewItem(itemText, activeItems.length));
-    event.target.value = "";
+    if (itemText !== "") {
+      allItems.push(getNewItem(itemText, allItems.length, "activeItemsColor"));
+      activeItems.add(allItems.length - 1);
+      renderItemsOn(focusedBtn.id);
+      event.target.value = "";
+    }
   }
 });
 
 //Event listener for the "Search" input
 searchElem.addEventListener("keyup", (event) => {
-  list.textContent = "";
+  list.textContent = ""; //remove all items from the "list"
   const enteredVal = event.target.value;
   const pattern = new RegExp(`^.*${enteredVal}.*$`, "i");
-  foundItems.length = 0;
-  activeItems.forEach((item) => {
-    if (pattern.test(item)) foundItems.push(item);
+  foundItems.clear();
+  allItems.forEach((item, index) => {
+    if (pattern.test(item.innerText)) foundItems.add(index);
   });
-  if (foundItems.length !== 0) renderItems(foundItems);
+  if (foundItems.size !== 0) renderItemsOn(focusedBtn.id, foundItems);
 });
 
 //Event listeners for buttons
 btnAllItems.addEventListener("click", () => {
   changeButtonStyle(btnAllItems);
-  renderItems(allItems);
+  renderItemsOn(focusedBtn.id);
 });
 btnCompletedItems.addEventListener("click", () => {
   changeButtonStyle(btnCompletedItems);
-  renderItems(completedItems);
+  renderItemsOn(focusedBtn.id);
 });
 btnActiveItems.addEventListener("click", () => {
   changeButtonStyle(btnActiveItems);
-  renderItems(activeItems);
+  renderItemsOn(focusedBtn.id);
 });
 
-//Function renders items in the list
-function renderItems(items) {
-  list.textContent = "";
-  items.forEach((item, index) => {
-    addItem(getNewItem(item, index));
-  });
+//Function renders items on the list, depending on the area focused in this moment.
+//It depends on the button that has class "focused" (btn IDs: "btnAllItems", "btnActiveItems", "btnCompletedItems").
+//It also consideres, whether the search was made and if it was, it renders only found items, depending on the area.
+function renderItemsOn(btnFocused, foundItems = false) {
+  list.textContent = ""; //remove all items from the "list"
+  switch (btnFocused) {
+    case "btnAllItems":
+      if (foundItems) {
+        allItems.forEach((item, index) => {
+          if (foundItems.has(index)) list.append(item);
+        });
+      } else {
+        allItems.forEach((item) => {
+          list.append(item);
+        });
+      }
+      break;
+    case "btnActiveItems":
+      if (foundItems) {
+        allItems.forEach((item, index) => {
+          if (foundItems.has(index) && activeItems.has(index))
+            list.append(item);
+        });
+      } else {
+        allItems.forEach((item, index) => {
+          if (activeItems.has(index)) list.append(item);
+        });
+      }
+      break;
+    case "btnCompletedItems":
+      if (foundItems) {
+        allItems.forEach((item, index) => {
+          if (foundItems.has(index) && completedItems.has(index))
+            list.append(item);
+        });
+      } else {
+        allItems.forEach((item, index) => {
+          if (completedItems.has(index)) list.append(item);
+        });
+      }
+      break;
+  }
 }
 
-//Function deletes items from the list
-function deleteItem(event) {
-  const id = event.target.id;
-  // activeItems = activeItems.filter((item, index) => index !== id);
-  const deletedItem = activeItems.splice(id, 1);
-  completedItems.push(deletedItem);
-  // document.getElementById(id).remove();
+//Function handles click on "list__item_checkBtn" button
+function completeItem(event) {
+  const id = Number(event.path[1].id);
+  completedItems.add(id);
+  activeItems.delete(id);
   document.getElementById(id).classList.add("completedItemsColor");
 }
 
 //Function creates new item before adding to the list
-function getNewItem(text, id) {
+function getNewItem(text, id, textStyleClass) {
   const rawItem = document.createElement("div");
+  const itemText = document.createElement("div");
+  const itemBtn = document.createElement("button");
   rawItem.classList.add("list__item");
+  rawItem.classList.add(textStyleClass);
   rawItem.setAttribute("id", id);
-  rawItem.addEventListener("click", deleteItem);
-  rawItem.innerHTML = text;
+  itemText.classList.add("list__item_text");
+  itemText.innerHTML = text;
+  itemBtn.classList.add("list__item_checkBtn");
+  itemBtn.addEventListener("click", completeItem);
+  rawItem.append(itemText, itemBtn);
   return rawItem;
 }
 
-//Function adds new item to the DOM
-function addItem(item) {
-  list.append(item);
-}
-
-//Function handles view of pressed button
+//Function handles view of pressed button and chnages reference to the "focused" button
 function changeButtonStyle(buttonName) {
   buttonName.classList.toggle("focused");
-  focusedItem.classList.toggle("focused");
-  focusedItem = buttonName;
+  focusedBtn.classList.toggle("focused");
+  focusedBtn = buttonName;
 }
-
-// switch (listName) {
-//   case allItems:
-//     rawItem.classList.add("allItemsColor");
-//     break;
-//   case btnActiveItems:
-//     rawItem.classList.add("activeItemsColor");
-//     break;
-//   case btnCompletedItems:
-//     rawItem.classList.add("completedItemsColor");
-//     break;
-// }
